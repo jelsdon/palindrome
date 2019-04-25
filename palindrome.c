87cbf02e888ea3e5d3a233fe7eb7ad0314161bc7
James Elsdon <Jamesys@gmail.com>
2019-04-26 09:23:09 +1000

    initial commit

---
+++ palindrome.c
@@ -0,0 +1,5 @@
+#include <string.h>//
+#include <stdio.h>//
+int main(int c,char **v){int i=0,l=0;l=strlen(v[1]);while(i<=l-1)putchar(v[1][i++]);i--;while(i-->=0)putchar(v[1][i]);printf("\n");0;}///};0;)"n\"(ftnirp;)]i[]1[v(rahctup)0=>--i(elihw;--i;)]++i[]1[v(rahctup)1-l=<i(elihw;)]1[v(nelrts=l;0=l,0=i tni{)v** rahc,c tni(niam tni
+//>h.oidts< edulcni#
+//>h.gnirts< edulcni#
