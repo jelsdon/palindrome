eeece154f75959b7adb1fee40217da5237f17993
afee <37291431+afee@users.noreply.github.com>
2019-10-25 19:05:39 +0530

    palindrome.c

--- palindrome.c
+++ palindrome.c
@@ -1,5 +1,20 @@
-#include <string.h>//
-#include <stdio.h>//
-int main(int c,char **v){int i=0,l=0;l=strlen(v[1]);while(i<=l-1)putchar(v[1][i++]);i--;while(i-->=0)putchar(v[1][i]);printf("\n");0;}///};0;)"n\"(ftnirp;)]i[]1[v(rahctup)0=>--i(elihw;--i;)]++i[]1[v(rahctup)1-l=<i(elihw;)]1[v(nelrts=l;0=l,0=i tni{)v** rahc,c tni(niam tni
-//>h.oidts< edulcni#
-//>h.gnirts< edulcni#
+#include <string.h>
+#include <stdio.h>
+int main(int c,char **v){
+  int i=0,l=0;l=strlen(v[1]);int j=0;int is_palindrome = 1;printf("String is\t"); 
+  while(i<=l-1)putchar(v[1][i++]);
+ printf("\n");printf("Reverse string is\t");
+  while(i-->0)putchar(v[1][i]);
+  printf("\n");
+for(i = 0, j = l - 1; i < l / 2; i++, j--) {
+        if(v[1][i] != v[1][j]) {
+            is_palindrome = 0;
+            break;
+        }
+    }
+
+    printf("String %s a palindrome.\n", is_palindrome ? "is" : "is not");
+
+    return(0);
+
+}
